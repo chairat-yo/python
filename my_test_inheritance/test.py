49a3df687106d0de888fea86a64189eae5554c20
chairat_rog <chaidev.cm@gmail.com>
2024-04-05 15:59:44 +0700

    add django

---
+++ my_test_inheritance/test.py
@@ -0,0 +1,18 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def swim(self):
+        print("Moving in water.")
+
+
+nemo = Fish()
+nemo.breathe()
