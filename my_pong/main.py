f58533eef097c85bf13edec8b62416467af1085f
chairat_rog <chaidev.cm@gmail.com>
2024-04-07 21:42:25 +0700

    completed

--- my_pong/main.py
+++ my_pong/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +12,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+score_board = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,8 +24,24 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_id_on = True
 while game_id_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
 
+    #Detect collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bound_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bound_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score_board.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score_board.r_point()
 
 
 screen.exitonclick()
