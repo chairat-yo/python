bd86e2412de57d52da26a4a6c5ecc85d4018bcc4
chairat_rog <chaidev.cm@gmail.com>
2024-04-18 10:42:07 +0700

    refactor dir

--- my_test_django/myproject1/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-def index(request):
-    name = "Yo"
-    country = "Thailand"
-    return render(request,"index.html", {"name": name, "country": country})
